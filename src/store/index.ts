a853fc50fe1b423fa7f850fd9732587e30e92572
Devlin D <webmasterdevlin>
2020-10-05 17:19:53 +0200

    fixed redux

--- src/store/index.ts
+++ src/store/index.ts
@@ -4,40 +4,16 @@ import {
 } from 'react-redux';
 import type { TypedUseSelectorHook } from 'react-redux';
 import type { ThunkAction } from 'redux-thunk';
-
 import type { Action } from '@reduxjs/toolkit';
 import { ENABLE_REDUX_DEV_TOOLS } from 'src/constants';
-import rootReducer, { createReducer } from './rootReducer';
-
-import createSagaMiddleware from 'redux-saga';
-import thunk from 'redux-thunk';
-import { heroSaga } from '../features/heroes/hero-saga';
+import rootReducer from './rootReducer';
 import { configureStore, getDefaultMiddleware } from '@reduxjs/toolkit';
-import { createInjectorsEnhancer, forceReducerReload } from 'redux-injectors';
-import logger from 'redux-logger';
-
-const reduxSagaMonitorOptions = {};
-const sagaMiddleware = createSagaMiddleware(reduxSagaMonitorOptions);
-const { run: runSaga } = sagaMiddleware;
-// Create the store with saga middleware
-const middlewares = [sagaMiddleware, thunk, logger];
-
-const enhancers = [
-  createInjectorsEnhancer({
-    createReducer,
-    runSaga,
-  }),
-];
-// Create the store with saga middleware
 
 const store = configureStore({
   reducer: rootReducer,
   middleware: [...getDefaultMiddleware()],
   devTools: ENABLE_REDUX_DEV_TOOLS,
-  enhancers,
 });
-// run Saga here
-runSaga(heroSaga);
 
 export type RootState = ReturnType<typeof store.getState>;
 
